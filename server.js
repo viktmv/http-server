7d6594d1b47e3da360a702c06afceb7d0bd34a09
Victor T <viktmv@icloud.com>
2017-10-08 11:59:36 -0400

    Remove dead code

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const PORT = 3000
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
 app.listen(PORT, () => {
   console.log('Example app listening on port 3000!')
 })
